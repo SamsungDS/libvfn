bc5d64ff1592fd07f3dd046c7de1cec094d8c800
Klaus Jensen <k.jensen@samsung.com>
2022-06-09 21:37:51 +0200

    src: add missing pthread.h includes
    Add missing includes of pthread.h.
    
    Signed-off-by: Klaus Jensen <k.jensen@samsung.com>

--- src/vfio/core.c
+++ src/vfio/core.c
@@ -21,6 +21,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #include <sys/ioctl.h>
 #include <sys/mman.h>
--- src/vfio/pci.c
+++ src/vfio/pci.c
@@ -21,6 +21,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
 
 #include <sys/ioctl.h>
 #include <sys/mman.h>
