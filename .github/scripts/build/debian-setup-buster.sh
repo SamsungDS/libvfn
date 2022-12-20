fee672f76fa78c1b8daa28636ae9abb84f0291da
Klaus Jensen <k.jensen@samsung.com>
2022-12-20 09:52:23 +0100

    github: upgrade pip before installing packages
    New packages require a newer version of pip.
    
    Signed-off-by: Klaus Jensen <k.jensen@samsung.com>

--- .github/scripts/build/debian-setup-buster.sh
+++ .github/scripts/build/debian-setup-buster.sh
@@ -3,4 +3,5 @@
 apt-get -y update
 apt-get -y install build-essential python3-pip
 
+pip3 install --upgrade pip
 pip3 install meson ninja
--- .github/scripts/build/rockylinux-setup-8.sh
+++ .github/scripts/build/rockylinux-setup-8.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 dnf -y install python3-pip gcc gcc-c++ perl
+
+pip3 install --upgrade pip
 pip3 install meson ninja
