SamsungDS/libvfn

commit 413ea5eb0d39401dbb87f499024de36b0c7df38c

queue: allow cq_wait_cqes to be called without timeout
Call the infinitely blocking (but slightly more performant)
nvme_cq_get_cqes() if the given timespec is NULL.

Signed-off-by: Klaus Jensen <k.jensen@samsung.com>

src/nvme/queue.c
@@ -56,13 +56,16 @@ void nvme_cq_get_cqes(struct nvme_cq *cq, struct nvme_cqe *cqes, int n)
 int nvme_cq_wait_cqes(struct nvme_cq *cq, struct nvme_cqe *cqes, int n, struct timespec *ts)
 {
 	struct nvme_cqe *cqe;
+	struct timerel rel = {.ts = *ts};
+	uint64_t timeout;
 
-	struct timerel rel = {
-		.ts = *ts,
-	};
+	if (!ts) {
+		nvme_cq_get_cqes(cq, cqes, n);
 
-	uint64_t timeout = get_ticks() +
-		time_to_usec(rel) * (__vfn_ticks_freq / 1000000ULL);
+		return 0;
+	}
+
+	timeout = get_ticks() + time_to_usec(rel) * (__vfn_ticks_freq / 1000000ULL);
 
 	do {
 		cqe = nvme_cq_get_cqe(cq);

commit 2374732aff33c0da3e7e3a7d36da24b8e37aabe7

nvme: fix nvme_cq_wait_cqes
nvme_cq_wait_cqes() is supposed to return the number of cqes reaped. If
no timeout is given, the function short-circuits to void
nvme_cq_get_cqes(), but unconditionally returns 0.

Also, it can't actually be called with the timeout parameter NULL.

Fix both issues.

Fixes: 413ea5eb0d39 ("queue: allow cq_wait_cqes to be called without timeout")
Signed-off-by: Klaus Jensen <k.jensen@samsung.com>

src/nvme/queue.c
@@ -56,14 +56,16 @@ void nvme_cq_get_cqes(struct nvme_cq *cq, struct nvme_cqe *cqes, int n)
 int nvme_cq_wait_cqes(struct nvme_cq *cq, struct nvme_cqe *cqes, int n, struct timespec *ts)
 {
 	struct nvme_cqe *cqe;
-	struct timerel rel = {.ts = *ts};
+	struct timerel rel;
 	uint64_t timeout;
 
 	if (!ts) {
 		nvme_cq_get_cqes(cq, cqes, n);
 
-		return 0;
+		return n;
 	}
+
+	rel.ts = *ts;
 
 	timeout = get_ticks() + time_to_usec(rel) * (__vfn_ticks_freq / 1000000ULL);
 