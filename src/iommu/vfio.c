SamsungDS/libvfn

commit cef9f6d60d054750c3b1040f2d5bca9c6c5c7b0d

iommu/vfio: fix get vfio group logic
We correctly get an existing group, but when we try to get the fd, we
always try to open the group again.

Fix this.

Reported-by: Haeun Kim <hanee.kim@samsung.com>
Signed-off-by: Klaus Jensen <k.jensen@samsung.com>

src/iommu/vfio.c
@@ -343,6 +343,9 @@ close_fd:
 static int vfio_get_group_fd(struct vfio_container *vfio,
 		struct vfio_group *group)
 {
+	if (group->fd != -1)
+		return group->fd;
+
 	group->fd = vfio_group_open(group->path);
 	if (group->fd < 0) {
 		log_debug("failed to open vfio group\n");
@@ -399,6 +402,8 @@ static struct vfio_group *__vfio_get_group(struct vfio_container *vfio,
 		if (!group->path) {
 			group->path = strdup(path);
 			group->nr_devs = 0;
+			group->fd = -1;
+
 			return group;
 		}
 	}

commit 6a8057c26674655f1a116f1d44f08574cd5570df

iommu/vfio: reset @group->fd when closing vfio group
When closing vfio group by a last owner, we should reset @group->fd = -1
as Commit cef9f6d60d05 ("iommu/vfio: fix get vfio group logic") started
checking @group->fd is -1 or not to give pre-open group fd back.
Otherwise, the following scenario will fail due to invalid file
descriptor which has already been closed at the next open time.

	vfio_pci_open()
	vfio_pci_close()
	vfio_pci_open()

Fixes: cef9f6d60d05 ("iommu/vfio: fix get vfio group logic")
Signed-off-by: Minwoo Im <minwoo.im@samsung.com>

src/iommu/vfio.c
@@ -477,6 +477,7 @@ static int vfio_put_device_fd(struct iommu_ctx *ctx, const char *bdf)
 
 	if (atomic_dec_fetch(&group->nr_devs) == 0) {
 		close(group->fd);
+		group->fd = -1;
 
 		/*
 		 * Kernel vfio driver will remove IOMMU driver and data from