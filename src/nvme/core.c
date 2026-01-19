SamsungDS/libvfn

commit 27607a2f05b06705b486817be9b7456cdd46aac8

nvme/core: adjust fixed doorbell mmio size
If application takes more than 512 sq/cq pairs, @ctrl->doorbells can't
simply represent the mmio region since it has fixed size of mmio
(0x1000).  To support large number of qpairs, calculate the proper mmio
doorbell size and map the proper size of it.  To do that, &struct
nvme_ctrl_opts is used to get the size of the doorbell possible.

Signed-off-by: Minwoo Im <minwoo.im@samsung.com>

src/nvme/core.c
@@ -616,8 +616,13 @@ put_doorbells:
 	return -1;
 }
 
-static int nvme_init_pci(struct nvme_ctrl *ctrl, const char *bdf)
+static int nvme_init_pci(struct nvme_ctrl *ctrl, const char *bdf,
+			 const struct nvme_ctrl_opts *opts)
 {
+	uint8_t dstrd;
+	uint64_t cap;
+	size_t size;
+
 	if (vfio_pci_open(&ctrl->pci, bdf) < 0 && errno != EALREADY)
 		return -1;
 
@@ -639,7 +644,11 @@ static int nvme_init_pci(struct nvme_ctrl *ctrl, const char *bdf)
 	}
 
 	/* map queue doorbells */
-	ctrl->doorbells = vfio_pci_map_bar(&ctrl->pci, 0, 0x1000, 0x1000, PROT_WRITE);
+	cap = le64_to_cpu(mmio_read64(ctrl->regs + NVME_REG_CAP));
+	dstrd = NVME_FIELD_GET(cap, CAP_DSTRD);
+	size = 2 * (max_t(int, opts->nsqr, opts->ncqr) + 2) * (4 << dstrd);
+
+	ctrl->doorbells = vfio_pci_map_bar(&ctrl->pci, 0, size, 0x1000, PROT_WRITE);
 	if (!ctrl->doorbells) {
 		log_debug("could not map doorbells\n");
 		return -1;
@@ -650,7 +659,7 @@ static int nvme_init_pci(struct nvme_ctrl *ctrl, const char *bdf)
 
 int nvme_pci_init(struct nvme_ctrl *ctrl, const char *bdf)
 {
-	return nvme_init_pci(ctrl, bdf);
+	return nvme_init_pci(ctrl, bdf, &nvme_ctrl_opts_default);
 }
 
 int nvme_ctrl_init(struct nvme_ctrl *ctrl, const char *bdf,
@@ -659,13 +668,13 @@ int nvme_ctrl_init(struct nvme_ctrl *ctrl, const char *bdf,
 	uint8_t mpsmin, mpsmax;
 	uint64_t cap;
 
-	if (nvme_init_pci(ctrl, bdf))
-		return -1;
-
 	if (opts)
 		memcpy(&ctrl->opts, opts, sizeof(*opts));
 	else
 		memcpy(&ctrl->opts, &nvme_ctrl_opts_default, sizeof(*opts));
+
+	if (nvme_init_pci(ctrl, bdf, &ctrl->opts))
+		return -1;
 
 	if ((ctrl->pci.classcode & 0xff) == 0x03)
 		ctrl->flags = NVME_CTRL_F_ADMINISTRATIVE;

commit c04170079ff10ef6db8b09765a0c88fcc8bce276

nvme/core: map doorbell mmio to as large as possible
Commit 27607a2f05b0 ("nvme/core: adjust fixed doorbell mmio size") has
adjusted the MMIO space to the maximum number of ioqpairs, but without
considering unmap part in nvme_close().

This patch gives SIZE_MAX to vfio/pci and let it map/unmap as large as
possible supported by the device for the simplicity.

Fixes: 27607a2f05b0 ("nvme/core: adjust fixed doorbell mmio size")

Signed-off-by: Minwoo Im <minwoo.im@samsung.com>

src/nvme/core.c
@@ -616,13 +616,8 @@ put_doorbells:
 	return -1;
 }
 
-static int nvme_init_pci(struct nvme_ctrl *ctrl, const char *bdf,
-			 const struct nvme_ctrl_opts *opts)
+static int nvme_init_pci(struct nvme_ctrl *ctrl, const char *bdf)
 {
-	uint8_t dstrd;
-	uint64_t cap;
-	size_t size;
-
 	if (vfio_pci_open(&ctrl->pci, bdf) < 0 && errno != EALREADY)
 		return -1;
 
@@ -644,11 +639,7 @@ static int nvme_init_pci(struct nvme_ctrl *ctrl, const char *bdf,
 	}
 
 	/* map queue doorbells */
-	cap = le64_to_cpu(mmio_read64(ctrl->regs + NVME_REG_CAP));
-	dstrd = NVME_FIELD_GET(cap, CAP_DSTRD);
-	size = 2 * (max_t(int, opts->nsqr, opts->ncqr) + 2) * (4 << dstrd);
-
-	ctrl->doorbells = vfio_pci_map_bar(&ctrl->pci, 0, size, 0x1000, PROT_WRITE);
+	ctrl->doorbells = vfio_pci_map_bar(&ctrl->pci, 0, SIZE_MAX, 0x1000, PROT_WRITE);
 	if (!ctrl->doorbells) {
 		log_debug("could not map doorbells\n");
 		return -1;
@@ -659,7 +650,7 @@ static int nvme_init_pci(struct nvme_ctrl *ctrl, const char *bdf,
 
 int nvme_pci_init(struct nvme_ctrl *ctrl, const char *bdf)
 {
-	return nvme_init_pci(ctrl, bdf, &nvme_ctrl_opts_default);
+	return nvme_init_pci(ctrl, bdf);
 }
 
 int nvme_ctrl_init(struct nvme_ctrl *ctrl, const char *bdf,
@@ -668,13 +659,13 @@ int nvme_ctrl_init(struct nvme_ctrl *ctrl, const char *bdf,
 	uint8_t mpsmin, mpsmax;
 	uint64_t cap;
 
+	if (nvme_init_pci(ctrl, bdf))
+		return -1;
+
 	if (opts)
 		memcpy(&ctrl->opts, opts, sizeof(*opts));
 	else
 		memcpy(&ctrl->opts, &nvme_ctrl_opts_default, sizeof(*opts));
-
-	if (nvme_init_pci(ctrl, bdf, &ctrl->opts))
-		return -1;
 
 	if ((ctrl->pci.classcode & 0xff) == 0x03)
 		ctrl->flags = NVME_CTRL_F_ADMINISTRATIVE;
@@ -805,7 +796,7 @@ void nvme_close(struct nvme_ctrl *ctrl)
 	}
 
 	vfio_pci_unmap_bar(&ctrl->pci, 0, ctrl->regs, 0x1000, 0);
-	vfio_pci_unmap_bar(&ctrl->pci, 0, ctrl->doorbells, 0x1000, 0x1000);
+	vfio_pci_unmap_bar(&ctrl->pci, 0, ctrl->doorbells, SIZE_MAX, 0x1000);
 
 	vfio_pci_close(&ctrl->pci);
 